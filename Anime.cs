8a3b09ac1ff89144c603a08c03995dc9495f6023
José Vinicius <76818599+tatsuyaBR2020@users.noreply.github.com>
2022-04-30 18:47:47 -0300

    Add files via upload

---
+++ Anime.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+public class Anime
+{
+    [BsonId]
+    public ObjectId ObjectId { get; set; }
+
+    [BsonElement("name")]
+    public string Name { get; set; }
+    [BsonElement("nota")]
+    public string Nota { get; set; }
+}
---
+++ MongoDB.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+
+public class MongoDBContext
+{
+    public bool TemAnime(string name)
+    {
+        var client = new MongoClient("mongodb+srv://Teste:Salve2022@animes.mho5n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        var database = client.GetDatabase("Animes");
+        var collection = database.GetCollection<Anime>("_animes");
+        return collection.Find<Anime>(g => g.Name == name).Count() > 0;
+    }
+}
